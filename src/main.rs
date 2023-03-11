fe1d75394be69bcd763cc44f8a71db77f8bd9c1c
Philip ks <3000piksel@gmail.com>
2023-03-11 20:10:08 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::env;
+use std::collections::VecDeque;
+
+mod util;
+
+fn main() {
+  // argument setup
+  let args: Vec<String> = env::args().collect();
+  let mut arg_depth: u8 = 0;
+  let mut args_sane: VecDeque<&str> = VecDeque::new();
+  let mut help: bool = false;
+  let mut error: bool = false;
+
+  // help output string
+  const HELP: &'static str = "Usage: yah [OPTION]...
+  Options:
+  h, help         print help
+  s, spotlight    launch spotlight utility
+  c, char         launch charpicker";
+
+  // process arguments
+  for arg in args.iter() {
+    if arg != &args[0] { // skip if executable
+      match arg.as_str() {
+        "h" | "help" => { help = true; break;}
+        "a" | "appLaunch" => {
+          if arg_depth == 0 {
+            arg_depth += 1;
+            args_sane.push_back("appLaunch");
+          } else {
+            error = true; break;
+          }
+        }
+        "s" | "scrLaunch" => {
+          if arg_depth == 0 {
+            arg_depth += 1;
+            args_sane.push_back("scrLaunch");
+          } else {
+            error = true; break;
+          }
+        }
+        "c" | "char" => {
+          if arg_depth == 0 {
+            arg_depth += 1;
+            args_sane.push_back("char");
+          } else {
+            error = true; break;
+          }
+        }
+        _ => { error = true; break; }
+      }
+    }
+  }
+
+  // run functionality
+  if error {
+    eprintln!("{}", HELP);
+  } else if help || args_sane.len() == 0 {
+    println!("{}", HELP);
+  } else {
+    match args_sane.pop_front() {
+      Some("appLaunch") => { util::launcher::launch_application(); }
+      Some("scrLaunch") => { util::launcher::launch_script(); }
+      Some("char") => { util::launcher::char(); }
+      Some(_) => { panic!("invalid element in args_sane"); }
+      None => { panic!("no elements in args_sane"); }
+    }
+  }
+}
---
+++ src/util/launcher.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+use std::io::Write;
+use std::process::{Command, Output, Stdio};
+
+fn dmenu<'a>(map: HashMap<&str, &'a str>) -> Option<&'a str> {
+  // setup dmenu command
+  let mut selector = Command::new("dmenu")
+    .args(["-c", "-l", "5"])
+    .stdin(Stdio::piped())
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("Failed to run dmenu.");
+
+  // pipe in keys
+  let mut keys: Vec<&str> = map.keys().map(|key| *key).collect();
+  keys.sort();
+  let keys_str: String = keys.join("\n");
+  let mut opt_in = selector.stdin.take().expect("Failed to open stdin.");
+  std::thread::spawn(move || {
+    opt_in.write_all(keys_str.as_bytes()).expect("Failed to write to stdin.");
+  });
+
+  // extract sanitized value from options
+  let raw_out: Output = selector.wait_with_output().expect("Failed to read stdout.");
+  let select_key: &str = std::str::from_utf8(raw_out.stdout.as_slice()).expect("Failed to convert from &[u8] to &str.");
+
+  // return selection
+  return if select_key != "" {
+    Some(&map.get(select_key.strip_suffix("\n").expect("Failed to strip newline.")).expect(""))
+  } else {
+    None
+  }
+}
+
+pub fn char() {
+  // Hashmap of possible options: (Key: "en_US char", Value: "special character")
+  let options: HashMap<&str, &str> = HashMap::from([
+    ("a", "ä"),
+    ("A", "Ä"),
+    ("o", "ö"),
+    ("O", "Ö"),
+    ("u", "ü"),
+    ("U", "Ü"),
+    ("sz", "ß"),
+    ("SZ", "ẞ"),
+  ]);
+
+  // use fancy dmenu & copy char if supplied
+  match dmenu(options) {
+    Some(selection) => {
+      let mut clip = Command::new("xclip")
+        .args(["-in", "-selection", "clipboard"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to run xclip.");
+
+      let mut opt_in = clip.stdin.take().expect("Failed to open stdin.");
+      std::thread::spawn(move || {
+        opt_in.write_all(selection.as_bytes()).expect("Failed to write to stdin.");
+      });
+      let raw_out: Output = clip.wait_with_output().expect("Failed to read stdout.");
+      let feedback: &str = std::str::from_utf8(raw_out.stdout.as_slice()).expect("Failed to convert from &[u8] to &str.");
+      println!("{}", feedback);
+    }
+    None => {  }
+  }
+
+}
+
+pub fn launch_script() {
+  // Hashmap of possible options: (Key: "command name", Value: "command")
+  let options: HashMap<&str, &str> = HashMap::from([
+    ("kill Xorg", "pkill Xorg"),
+    ("kill chromium", "pkill chromium"),
+    ("poweroff", "sudo poweroff"),
+    ("reboot", "sudo reboot"),
+  ]);
+
+  // use fancy dmenu & launch output if supplied
+  match dmenu(options) {
+    Some(selection) => { Command::new("sh").args(["-c", selection]).output().expect("Failed to launch."); }
+    None => {  }
+  }
+}
+
+pub fn launch_application() {
+  // Hashmap of possible options: (Key: "program", Value: "launch command")
+  let options: HashMap<&str, &str> = HashMap::from([
+    ("arandr", "arandr"),
+    ("bleachbit", "bleachbit"),
+    ("chromium", "chromium"),
+    ("clion","clion"),
+    ("datagrip", "datagrip"),
+    ("discord", "discord"),
+    ("gimp", "gimp"),
+    ("inkscape", "inkscape"),
+    ("lxappearance", "lxappearance"),
+    ("nvidia", "nvidia-settings"),
+    ("order66", "killall chromium"),
+    ("piper", "piper"),
+    ("prismlauncher", "prismlauncher"),
+    ("pycharm","pycharm"),
+    ("spotify", "spotify"),
+    ("tor", "tor-browser"),
+    ("whatsapp", "whatsapp-nativefier"),
+  ]);
+
+  // use fancy dmenu & launch output if supplied
+  match dmenu(options) {
+    Some(selection) => { Command::new(selection).spawn().expect("Failed to launch."); }
+    None => {  }
+  }
+}
