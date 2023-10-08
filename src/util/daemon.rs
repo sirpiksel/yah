eb737feb752e5a833171e8745e0e7334c0ee5e5f
Philip ks <3000piksel@gmail.com>
2023-10-08 14:17:31 +0200

    add sound notification

--- src/util/daemon.rs
+++ src/util/daemon.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env;
 use std::fs::{self, create_dir_all, File, set_permissions};
 use std::io;
 use std::io::{Read, Write};
@@ -6,37 +7,14 @@ use std::os::fd::AsRawFd;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 use std::process;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
+use alto::{Alto, Mono, Source, Stereo};
 use chrono::Local;
 use chrono::NaiveTime;
-
-use crate::env;
-
-pub fn daemon(options: &HashMap<String, String>) {
-  // Fork the current process to create a child process
-  match unsafe { libc::fork() } {
-    -1 => {
-      eprintln!("Fork failed.");
-      process::exit(1);
-    }
-    0 => unsafe {
-      // Setup daemon
-      daemonize();
-      create_cronux_pipe().expect("Failed to create pipe");
-
-      // Perform tasks here
-      loop {
-        write_cronux_pipe(options).expect("Failed to write to pipe");
-
-        std::thread::sleep(std::time::Duration::from_secs(60));
-      }
-    }
-    _ => {
-      // Parent process: Exit
-      process::exit(0);
-    }
-  }
-}
+use lewton::inside_ogg::OggStreamReader;
+use lewton::VorbisError;
 
 unsafe fn daemonize() {
   // Get XDG_CONFIG_HOME environment variable
@@ -86,18 +64,26 @@ fn dup2(src: std::os::unix::io::RawFd, dst: std::os::unix::io::RawFd) -> io::Res
   }
 }
 
-fn create_cronux_pipe() -> io::Result<()> {
+fn create_cronux_config() -> io::Result<()> {
   let pipe_path = env::current_dir()?.join("cronux");
+  let sound_path = env::current_dir()?.join("sound.ogg");
+
+  // Get the sound file
+  let sound_bytes = include_bytes!("../../sound.ogg");
 
   // Check if the named pipe already exists.
-  if pipe_path.exists() {
-    return Ok(());
+  if !pipe_path.exists() {
+    // Create the named pipe with the FileType::Fifo type.
+    File::create(&pipe_path)?;
+    set_permissions(&pipe_path, fs::Permissions::from_mode(0o644))?;
   }
 
-  // Create the named pipe with the FileType::Fifo type.
-  create_dir_all(&pipe_path.parent().unwrap())?; // Ensure parent directory exists.
-  File::create(&pipe_path)?;
-  set_permissions(&pipe_path, fs::Permissions::from_mode(0o666))?; // Set permissions as needed.
+  // Check if sound file already exists.
+  if !sound_path.exists() {
+    // Create the sound file
+    fs::write(&sound_path, sound_bytes)?;
+    set_permissions(&sound_path, fs::Permissions::from_mode(0o644))?;
+  }
 
   Ok(())
 }
@@ -124,8 +110,12 @@ fn write_cronux_pipe(timetable: &HashMap<String, String>) -> io::Result<()> {
   }
 
   // play sound if the activity just changed
-  //if current_time == activity_time {
-  //}
+  if current_time == activity_time {
+    match play_sound() {
+      Ok(_) => (),
+      Err(err) => eprintln!("Failed to play sound: {}", err),
+    }
+  }
 
   // Open the named pipe "cronux" in write-only mode
   let pipe_path = Path::new("./cronux");
@@ -143,6 +133,91 @@ fn write_cronux_pipe(timetable: &HashMap<String, String>) -> io::Result<()> {
   Ok(())
 }
 
+fn play_sound() -> Result<(), VorbisError> {
+  let f = File::open("sound.ogg").expect("Failed to open sound file");
+
+  // Prepare the reading
+  let mut srr = OggStreamReader::new(f)?;
+
+  // Prepare the playback.
+  let al = Alto::load_default().expect("Could not load alto");
+  let device = al.open(None).expect("Could not open device");
+  let cxt = device.new_context(None).expect("Could not create context");
+  let mut str_src = cxt.new_streaming_source()
+    .expect("could not create streaming src");
+  let sample_rate = srr.ident_hdr.audio_sample_rate as i32;
+
+  if srr.ident_hdr.audio_channels > 2 {
+    // the openal crate can't process these many channels directly
+    println!("Stream error: {} channels are too many!", srr.ident_hdr.audio_channels);
+  }
+
+  // Decode
+  let mut n = 0;
+  let mut len_play = 0.0;
+  let mut start_play_time = None;
+  let start_decode_time = Instant::now();
+  let sample_channels = srr.ident_hdr.audio_channels as f32 *
+    srr.ident_hdr.audio_sample_rate as f32;
+  while let Some(pck_samples) = srr.read_dec_packet_itl()? {
+    n += 1;
+    let buf = match srr.ident_hdr.audio_channels {
+      1 => cxt.new_buffer::<Mono<i16>, _>(&pck_samples, sample_rate),
+      2 => cxt.new_buffer::<Stereo<i16>, _>(&pck_samples, sample_rate),
+      n => panic!("unsupported number of channels: {}", n),
+    }.unwrap();
+
+    str_src.queue_buffer(buf).unwrap();
+
+    // Play
+    len_play += pck_samples.len() as f32 / sample_channels;
+    // If the decode is faster than realtime start playing now.
+    if n == 100 {
+      let cur = Instant::now();
+      if cur - start_decode_time < Duration::from_millis((len_play * 1000.0) as u64) {
+        start_play_time = Some(cur);
+        str_src.play();
+      }
+    }
+  }
+  let total_duration = Duration::from_millis((len_play * 1000.0) as u64);
+  let sleep_duration = total_duration - match start_play_time {
+    None => {
+      str_src.play();
+      Duration::from_millis(0)
+    }
+    Some(t) => Instant::now() - t,
+  };
+  sleep(sleep_duration);
+
+  Ok(())
+}
+
+pub fn daemon(options: &HashMap<String, String>) {
+  // Fork the current process to create a child process
+  match unsafe { libc::fork() } {
+    -1 => {
+      eprintln!("Fork failed.");
+      process::exit(1);
+    }
+    0 => unsafe {
+      // Setup daemon
+      daemonize();
+      create_cronux_config().expect("Failed to create pipe");
+
+      // Perform tasks here
+      loop {
+        write_cronux_pipe(options).expect("Failed to write to pipe");
+
+        sleep(Duration::from_secs(60));
+      }
+    }
+    _ => {
+      // Parent process: Exit
+      process::exit(0);
+    }
+  }
+}
 
 pub fn cronux() {
   let env_config = match env::var("XDG_CONFIG_HOME") {
